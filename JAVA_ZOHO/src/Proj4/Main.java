349b6aeef3ef312e4b4ebe7f21e210d3ea747427
cyberkid05 <cyberadithya05@gmail.com>
2022-03-02 17:18:25 +0530

    Add files via upload

---
+++ JAVA_ZOHO/src/Proj4/Main.java
@@ -0,0 +1,143 @@
+package Proj4;	// if Editor == Sublime: Comment this line
+
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+import java.util.Set;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		// File file_object = new File("E:\\Coding\\Dev\\Inputf.txt");
+		// Scanner fscan = new Scanner(file_object);
+		// BufferedReader fbr = new BufferedReader(new InputStreamReader(file_object));
+		PrintStream ss = System.out;
+		Scanner scan = new Scanner(System.in);
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		SimpleDateFormat dateformat1 = new SimpleDateFormat("dd/MM/yyyy");
+		try {
+			// ss.println("hello world");
+		} catch (Exception e) {
+			ss.println(e); // ss.println("Some Error occurred");
+		}
+
+		Map<String, User> userlist = new HashMap<>();    // unique usernames
+
+		int choice = 1;
+		do {
+			try {
+				ss.println("Enter your choice: ");
+				ss.println("1. Insert new user");
+				ss.println("2. Delete existing user");
+				ss.println("3. Update existing user");
+				ss.println("4. Display users details");
+				ss.println("5. Search user by username");
+				ss.println("6. Show all users details");
+				ss.println("0. Exit");
+				choice = scan.nextInt();
+				scan.nextLine();    // strip("\t\n")
+			} catch (Exception e) {
+				System.out.println("Invalid Input :(");
+				break;
+			}
+			switch (choice) {
+			case 0: {
+				choice = 0;
+				break;
+			}
+			case 1: {
+				ss.println("First Name: ");
+				String fname = scan.nextLine();
+				ss.println("Last Name: ");
+				String lname = scan.nextLine();
+				ss.println("User Name: ");
+				String uname = scan.nextLine();
+				ss.println("Password: ");
+				String pwd = scan.nextLine();
+				ss.println("City: ");
+				String city = scan.nextLine();
+				ss.println("Email Address: ");
+				String email = scan.nextLine();
+				ss.println("Join Date: ");
+				String doj = scan.nextLine();
+
+				User newaccount = new User(fname, lname, uname, pwd, city, email, doj);
+				userlist.put(newaccount.getUname(), newaccount);
+				ss.println(userlist);
+				// user
+				break;
+			}
+			case 2: {
+				ss.println("Enter Username: ");
+				String uname = scan.nextLine();
+				userlist.remove(uname);
+				break;
+			}
+			case 3: {
+				ss.println("Username: ");
+				String queryuser = scan.nextLine();
+				if (userlist.containsKey(queryuser)) {
+					ss.println("What to update?");
+					ss.println("1.pwd\t2.city\t3.email");
+					int query2 = scan.nextInt();
+					scan.nextLine();    // strip("\t\n")
+					switch (query2) {
+					case 1: {
+						ss.println("New Password: ");
+						String pwd = scan.nextLine();
+						userlist.get(queryuser).setPwd(pwd);
+						break;
+					}
+					case 2: {
+						ss.println("New City: ");
+						String city = scan.nextLine();
+						userlist.get(queryuser).setCity(city);
+						break;
+					}
+					case 3: {
+						ss.println("New Email Address: ");
+						String email = scan.nextLine();
+						userlist.get(queryuser).setEmail(email);
+						break;
+					}
+					default: {
+						ss.println("Invalid Input");
+					}
+					}
+				}
+				break;
+			}
+			case 4: {
+				for (Map.Entry<String, User> usr : userlist.entrySet()) {
+					ss.println(
+					    usr.getKey() + "-" +
+					    usr.getValue().getFname() + " " +
+					    usr.getValue().getLname() + "-" +
+					    usr.getValue().getEmail()
+					);
+				}
+				break;
+			}
+			case 5: {
+				ss.println("Username: ");
+				String queryuser = scan.nextLine();
+				if (userlist.containsKey(queryuser)) {
+					ss.println(userlist.get(queryuser).getFullname());
+				} else {
+					ss.println("User not found.");
+				}
+				break;
+			}
+			case 6: {
+				ss.println(userlist);
+				break;
+			}
+			default: {
+				ss.println("Invalid choice :(");
+				break;
+			}
+			}
+		} while (choice != 0);
+	}
+}
