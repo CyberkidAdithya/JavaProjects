e04dd7f61b659b355ef05dc2de7ac7d39070084b
cyberkid05 <cyberadithya05@gmail.com>
2022-03-10 12:10:24 +0530

    More Exception Handling - 1

--- JAVA_ZOHO/src/Proj0/Application0.java
+++ JAVA_ZOHO/src/Proj0/Application0.java
@@ -17,11 +17,12 @@ public class Application0 {
         String abs_class_name = Application0.class.getName();
         String class_name = abs_class_name.substring(1 + abs_class_name.indexOf("."));
         String package_name = abs_class_name.substring(0, abs_class_name.indexOf("."));
-        System.out.println(class_name);
-        System.out.println(package_name);
+        ss.println(class_name);     // DEBUG
+        ss.println(package_name);   // DEBUG
         File file_object = new File(file_path + "\\src\\" + package_name + "\\input.in");
-        if (!file_object.exists()) {
-            System.out.println("Failed to load the input file!");
+        boolean readfromfile = file_object.exists();
+        if (!readfromfile) {
+            ss.println("Failed to load the input file!");
             scan = new Scanner(System.in);
             BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         } else {
@@ -29,35 +30,52 @@ public class Application0 {
             BufferedReader br = new BufferedReader(new FileReader(file_object));    // input file and cmd
         }
 
-        ArrayList<Student> cseA = new ArrayList<>();
-//        ss.println("Number of Students: ");
-//        int numberofstudents = scan.nextInt();
-//        scan.nextLine();
-        while (scan.hasNext()) {
-            ss.println("Student Name: ");
-            String newname = scan.next();
-            ss.println("Student Marks (format: mark1, mark2, mark3): ");
-            int[] newmarks = new int[3];
-            for (int i = 0; i < 3; i++) {
-                newmarks[i] = scan.nextInt();
+        try {
+            ArrayList<Student> cseA = new ArrayList<>();
+            int numberofstudents = 0;
+            while (scan.hasNext()) {
+                ss.print(!readfromfile ? "\nStudent Name: " : "");
+                String newname = scan.next();
+                ss.println(!readfromfile ? "\nStudent Marks (format: mark1, mark2, mark3): " : "");
+                int[] newmarks = new int[3];
+                for (int i = 0; i < 3; i++) {
+                    newmarks[i] = scan.nextInt();
+                }
+                Student newcomer = new Student(newname, newmarks);
+                cseA.add(newcomer);
+                numberofstudents++;
             }
-            Student newcomer = new Student(newname, newmarks);
-            cseA.add(newcomer);
-        }
-        ss.println(cseA);
-        ss.println("Enter roll no. to query:\t");
-        int queryindex = scan2.nextInt();
-        boolean found = false;
-        for (Student stud: cseA) {
-            if (stud.getId() == queryindex) {
-                ss.println("Result of " + cseA.get(queryindex).getName() + ": " + (Student.getaverage(cseA.get(queryindex).getMarks()) > 40 ? "pass" : "fail"));      // class.function(object.variable)
-                found = true;
-                break;
+            ss.println(cseA);
+            ss.println("Enter roll no. to query:\t");
+
+            int queryindex = -1;
+            try {
+                queryindex = scan2.nextInt();
+            } catch (Exception err) {
+                ss.println("Invalid Input given: " + err);
+            }
+            boolean found = false;
+            for (Student stud : cseA) {      // Search Records
+                if (queryindex > numberofstudents) {
+                    ss.println("Invalid Index Given");
+                    break;
+                }
+                if (stud.getId() == queryindex) {
+                    // class.function(object.variable)
+                    ss.println("Result of " + stud.getName() + ": " + (Student.getaverage(stud.getMarks()) > 40 ? "pass" : "fail"));
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) {
+                ss.println("Student not found!");
             }
         }
-        if (!found) {
-            ss.println("Student not found");
+        catch (Exception err) {
+            System.out.println("Program crashed successfully: " + err);
         }
 
+        scan.close();
+        scan2.close();
     }
 }
